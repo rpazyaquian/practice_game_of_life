4d76a87efb4355654d966206ac597400ec95760b
Rebecca Paz <r.pazyaquian@gmail.com>
2014-10-28 11:07:09 -0400

    adding gemfile, generated steps

---
+++ features/step_definitions/game_of_life_steps.rb
@@ -0,0 +1,22 @@
+Given(/^the following setup$/) do |table|
+  # table is a Cucumber::Ast::Table
+  # pending # express the regexp above with the code you wish you had
+
+end
+
+When(/^I evolve the board$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the center cell should be dead$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^the center cell should be alive$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see the following board$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
